a964fde98618c2fb78f7e5d192f840ddeb1380b8
jjuuuunnii <129721531+jjuuuunnii@users.noreply.github.com>
2024-03-03 18:51:35 +0900

    Init: first commit (#2)

---
+++ src/test/java/org/dongguk/mlac/MlacApplicationTests.java
@@ -0,0 +1,13 @@
+package org.dongguk.mlac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MlacApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
